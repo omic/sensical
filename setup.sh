72e7dcae7e5425050afa4bc55c07a822f9be87ff
Luke Weber <lukedottec@gmail.com>
2020-06-11 20:07:31 +0000

    Commit changes before AWS EBS storage upgrade.

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 # Hydrate and unzip all COVID-19 tweets.
 # 
 # NOTE:  The total required space, after decompression, is over 20GB.
-python3 -m pip install twarc
-twarc configure
+#python3 -m pip install twarc
+#twarc configure
 cd tweets/
 ./hydrate.py
 gunzip ./2020-*/*.gz
